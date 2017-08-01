f0f82ec83debef0ae3731e3098065d446dc9535f
fucongcong <fucongcong@clothesmake.com>
2017-08-01 17:25:11 +0800

    add

--- C/demo/socket.c
+++ C/demo/socket.c
@@ -1,47 +1,58 @@
 #include <sys/socket.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <string.h>
+#include <stdio.h>
 
 int main()
 {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if(-1 == fd)
     {
-      perror("can not create socket");
-      exit(EXIT_FAILURE);
+        perror("can not create socket");
+        exit(EXIT_FAILURE);
     }
-
-    struct sockaddr_in serv_addr;
+    
+    struct sockaddr_in serv_addr,client_addr;
     char host[] = "127.0.0.1";
     short port = 1234;
     short buf_size = 2;
-    char eof = "-";
-
-    memset(&serv_addr, 0, sizeof(serv_addr));
+    int len;
+    char clientIp[20];
+    int res = 1;
+    char buffer[buf_size];  //缓冲区
+    char recvBuff[1024000];
+    
+    bzero(&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    if (!inet_aton(host, &serv_addr.sin_addr.s_addr)) {
+    // if (!inet_aton(host, &serv_addr.sin_addr.s_addr)) {
+    //     perror("bad address");
+    //     exit(EXIT_FAILURE);
+    // }
+    if (inet_pton(AF_INET, host, &serv_addr.sin_addr) <= 0) {
         perror("bad address");
         exit(EXIT_FAILURE);
     }
+    
     serv_addr.sin_port = htons(port);
     if(-1 == bind(fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) {
         perror("bind address fail");
         close(fd);
         exit(EXIT_FAILURE);
     }
-
+    
     if(-1 == listen(fd, 10))
     {
         perror("error listen failed");
         close(fd);
         exit(EXIT_FAILURE);
     }
-
+    
     for(;;)
     {
-        int clientFd = accept(fd, NULL, NULL);
+        len = sizeof(client_addr);
+        int clientFd = accept(fd, (struct sockaddr*)&client_addr, &len);
 
         if(0 > clientFd)
         {
@@ -49,15 +60,15 @@ int main()
             close(fd);
             exit(EXIT_FAILURE);
         }
-
-        int res = 1;
-        char buffer[buf_size];  //缓冲区
-        char recvBuff[1024000];
-
-        //char buf[buf_size];
+        
+        printf("connection from %s, port %d\n",
+               inet_ntop(AF_INET, &client_addr.sin_addr, clientIp, sizeof(clientIp)),
+               ntohs(client_addr.sin_port));
+        
         while(res) {
             int strLen = recv(clientFd, buffer, buf_size, 0);  //接收客户端发来的数据
             strcat(recvBuff, buffer);
+ 
             if (strLen < sizeof(buffer)) {
                 break;
             } else {
@@ -66,13 +77,13 @@ int main()
         }
         
         printf("%s\n", recvBuff);
-        memset(&recvBuff, 0, sizeof(recvBuff));
-
+        bzero(&recvBuff, sizeof(recvBuff));
+        
         shutdown(clientFd, SHUT_RDWR);
         close(clientFd);
         //send(clientFd, buffer, strLen, 0);  //将数据原样返回
+        
     }
-
     close(fd);
     return 0;
 }
