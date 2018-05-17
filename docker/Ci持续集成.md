ef9f331d6d6eb063aac84350f1e0819975ba6f61
fucongcong <fucongcong@clothesmake.com>
2018-05-17 16:33:17 +0800

    ssos

---
+++ docker/Ci持续集成.md
@@ -0,0 +1,3 @@
+#### 安装gitlab-runner
+#### 编写gitlab-ci.yml 
+#### 将docker私有的key（通过 docker login,获取~/.docker/config.json配置获得） DOCKER_AUTH_CONFIG填写到gitlab的 secret var参数中
--- docker/init.sh
+++ docker/init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-service nginx start
-service php5-fpm start
-service mysql start
+# service nginx start
+# service php5-fpm start
+# service mysql start
+apt-get -yqq update
+apt-get -yqq install git
