dcd8e6c36b6d706295d5965330bd14bbd7bf2520
fucongcong <664985936@qq.com>
2019-01-11 16:47:03 +0800

    新增list类型

--- 系统编程/网络通信之简单的通信协议多语言Demo/php/demo/demo3.php
+++ 系统编程/网络通信之简单的通信协议多语言Demo/php/demo/demo3.php
@@ -1,56 +1,77 @@
 <?php 
 
-// struct Members
-// {
-//     0 need String names;
-// }
-// struct Company
-// {
-//     0 need int id;
-//     1 need long code;
-//     2 need string name;
-//     16 optional string addr;
-//     4 optional Members members;
-// }
-// 
-// struct User
-// {
-//     0 need int id;
-//     1 optional Company company;
-// }
-
 require __DIR__.'/../lib/Co.php';
 
 class Members extends CoStruct {
     public $names;
+    public $sexs;
+    public $work = false;
+
+    public function __clone()
+    {
+        $this->names = clone $this->names;
+    }
 
     public function __construct()
     {
-        $this->names = new CoString;
+        $this->names = new CoList(new CoString);
+        // $this->sexs = new CoMap(new CoString, new CoString);
+        // $this->work = new CoBool;
     }
 
-    public function setNames(string $names)
+    public function setNames($names)
     {
         $this->names->setVal($names);
+    }
+
+    public function setSexs($sexs)
+    {
+        $this->sexs->setVal($sexs);
+    }
+
+    public function setWork($work)
+    {
+        $this->work->setVal($work);
     }
 
     public function pack(&$val)
     {
         $this->names->get($val, 0, true);
+        // $this->sexs->get($val, 1, false);
+        // $this->work->get($val, 2, false);
     }
 
     public function unpack($data)
-    {   
-        $this->names = $this->names->set($data, 0);
+    {
+        $this->names = $this->names->getUnpackData($data, 0);
+        // $this->sexs = $this->sexs->getUnpackData($data, 1);
+        // $this->work = $this->work->getUnpackData($data, 2);
     }
 }
 
 class Company extends CoStruct {
+
     public $id;
+
     public $code;
+
     public $name;
+
     public $addr;
+
     public $members;
+
+    public $memberLists;
+
+    public function __clone()
+    {
+        $this->id = clone $this->id;
+        $this->code = clone $this->code;
+        $this->name = clone $this->name;
+        $this->addr = clone $this->addr;
+        $this->members = clone $this->members;
+        $this->memberLists = clone $this->memberLists;
+    }
 
     public function __construct()
     {
@@ -59,9 +80,10 @@ class Company extends CoStruct {
         $this->name = new CoString;
         $this->addr = new CoString;
         $this->members = new Members;
+        $this->memberLists = new CoList(new Members);
     }
 
-    public function setId(int $id)
+    public function setId($id)
     {
         $this->id->setVal($id);
     }
@@ -86,6 +108,11 @@ class Company extends CoStruct {
         $this->members->setVal($members);
     }
 
+    public function setMemberLists($memberLists)
+    {
+        $this->memberLists->setVal($memberLists);
+    }
+
     public function pack(&$val)
     {
         $this->id->get($val, 0, true);
@@ -93,15 +120,17 @@ class Company extends CoStruct {
         $this->name->get($val, 2, true);
         $this->addr->get($val, 16, false);
         $this->members->get($val, 4, false);
+        $this->memberLists->get($val, 5, false); 
     }
 
     public function unpack($data)
     {   
-        $this->id = $this->id->set($data, 0);
-        $this->code = $this->code->set($data, 1);
-        $this->name = $this->name->set($data, 2);
-        $this->addr = $this->addr->set($data, 16);
-        $this->members = $this->members->set($data, new Members, 4);
+        $this->id = $this->id->getUnpackData($data, 0);
+        $this->code = $this->code->getUnpackData($data, 1);
+        $this->name = $this->name->getUnpackData($data, 2);
+        $this->addr = $this->addr->getUnpackData($data, 16);
+        $this->members = $this->members->getUnpackData($data, new Members, 4);
+        $this->memberLists = $this->memberLists->getUnpackData($data, 5);
     }
 }
 
@@ -109,13 +138,19 @@ class User extends CoStruct {
     public $id;
     public $company;
 
+    public function __clone()
+    {
+        $this->id = clone $this->id;
+        $this->company = clone $this->company;
+    }
+
     public function __construct()
     {
         $this->id = new CoInt;
         $this->company = new Company;
     }
 
-    public function setId(int $id)
+    public function setId($id)
     {
         $this->id->setVal($id);
     }
@@ -133,16 +168,38 @@ class User extends CoStruct {
 
     public function unpack($data)
     {   
-        $this->id = $this->id->set($data, 0);
-        $this->company = $this->company->set($data, new Company, 1);
+        $this->id = $this->id->getUnpackData($data, 0);
+        $this->company = $this->company->getUnpackData($data, new Company, 1);
     }
 }
 
+// struct Members
+// {
+//     0 need list<string> names;
+// }
+// struct Company
+// {
+//     0 need int id;
+//     1 need long code;
+//     2 need string name;
+//     16 optional string addr;
+//     4 optional Members members;
+//     5 optional list<Members> memberLists;
+// }
+// 
+// struct User
+// {
+//     0 need int id;
+//     1 optional Company company;
+// }
 
 $data = null;
 
 $members = new Members;
-$members->setNames("coco,fucongcong.jason");
+$members->setNames(["aa", "cc", "dd"]);
+
+$members2 = new Members;
+$members2->setNames(["coco"]);
 
 $company = new Company;
 $company->setId(10);
@@ -150,6 +207,7 @@ $company->setCode(1111);
 $company->setName('coco');
 $company->setAddr('aaaaaaaaaa');
 $company->setMembers($members);
+$company->setMemberLists([$members, $members2]);
 
 $user = new User;
 $user->setId(1);
@@ -158,23 +216,23 @@ $user->pack($data);
 
 echo "压缩后总长度为".strlen($data)."\n";
 
-$unpackData = coPack($data);
+$unpackData = coUnpack($data);
 
 $obj = new User;
 $obj->unpack($unpackData);
 print_r($obj);
 
 
-$data = [
-    'id' => 1,
-    'company' => [
-        'id' => 10,
-        'code' => 1111,
-        'name' => 'coco',
-        'addr' => 'aaaaaaaaaa',
-        'members' => [
-            'names' => 'coco,fucongcong.jason'
-        ]
-    ]
-];
-echo "json序列化长度为".strlen(json_encode($data))."\n";
+// $data = [
+//     'id' => 1,
+//     'company' => [
+//         'id' => 10,
+//         'code' => 1111,
+//         'name' => 'coco',
+//         'addr' => 'aaaaaaaaaa',
+//         'members' => [
+//             'names' => 'coco,fucongcong.jason'
+//         ]
+//     ]
+// ];
+// echo "json序列化长度为".strlen(json_encode($data))."\n";
--- 系统编程/网络通信之简单的通信协议多语言Demo/php/lib/Co.php
+++ 系统编程/网络通信之简单的通信协议多语言Demo/php/lib/Co.php
@@ -1,6 +1,6 @@
 <?php 
 
-function coPack($data)
+function coUnpack($data)
 {
     $len = 0;
     $unpackData = [];
@@ -35,42 +35,71 @@ function coPack($data)
     return $unpackData;
 }
 
+function coPack($data, $type, $tag, &$val)
+{
+    if ($tag >= 15) {
+        $val .= chr(($type << 4) | 15);
+        $val .= chr($tag).$data;
+    } else {
+        $val .= chr(($type << 4) | $tag).$data;
+    }
+}
+
 class CoType
 {
     const CO_INT = 1;
     const CO_LONG = 2;
     const CO_STRING = 3;
     const CO_STRUCT = 4;
+    const CO_LIST = 5;
+    const CO_BOOL = 6;
+    const CO_MAP = 7;
 }
 
 class DataType 
 {   
-    public function getVal(&$val, $type, $tag, $isNeed = true)
+    public function packVal(&$val, $type, $tag, $isNeed = true)
     {   
         if (!isset($this->val)) {
             $this->val = null;
         }
-        
+
         if ($this->val != null) {
-            if ($tag >= 15) {
-                $val .= chr(($type << 4) | 15);
-                $val .= chr($tag).$this->val;
-            } else {
-                $val .= chr(($type << 4) | $tag).$this->val;
-            }
+            coPack($this->val, $type, $tag, $val);
         } else {
             if ($isNeed) {
-                throw new Exception("缺少必要的参数标记为:{$tag}", 1); 
+                throw new Exception("缺少必要的参数标记为:{$tag}", 1);
             }
         }
     }
 
     public function setVal($data)
     {
-        $this->val =  pack("a*", $data);
+        $this->val = pack("a*", $data);
         $bodyLen = strlen($this->val);
         $head = pack("N", $bodyLen);
         $this->val = $head.$this->val;
+        return $this->val;
+    }
+
+    public function getVal($data)
+    {
+        $len = 0;
+        $ret = [];
+        while (strlen($data) - $len > 0) {
+
+            if ($len > 0) $data = substr($data, $len);
+            $len = unpack("N", substr($data, 0, 4));
+            $len = $len[1];
+
+            $data = substr($data, 4);
+            $val = unpack("a*", substr($data, 0, $len));
+            $val = $val[1];
+
+            $ret[] = $val;
+        }
+
+        return $ret;
     }
 }
 
@@ -78,10 +107,10 @@ class CoInt extends DataType
 {   
     public function get(&$val, $tag, $isNeed = true)
     {
-        $this->getVal($val, CoType::CO_INT, $tag, $isNeed);
+        $this->packVal($val, CoType::CO_INT, $tag, $isNeed);
     }
 
-    public function set($data, $tag)
+    public function getUnpackData($data, $tag)
     {
         if (isset($data[$tag]) && $data[$tag]['type'] == CoType::CO_INT) {
             return $data[$tag]['val'];
@@ -93,10 +122,10 @@ class CoLong extends DataType
 {   
     public function get(&$val, $tag, $isNeed = true)
     {
-        $this->getVal($val, CoType::CO_LONG, $tag, $isNeed);
+        $this->packVal($val, CoType::CO_LONG, $tag, $isNeed);
     }
 
-    public function set($data, $tag)
+    public function getUnpackData($data, $tag)
     {
         if (isset($data[$tag]) && $data[$tag]['type'] == CoType::CO_LONG) {
             return $data[$tag]['val'];
@@ -108,13 +137,81 @@ class CoString extends DataType
 {   
     public function get(&$val, $tag, $isNeed = true)
     {
-        $this->getVal($val, CoType::CO_STRING, $tag, $isNeed);
+        $this->packVal($val, CoType::CO_STRING, $tag, $isNeed);
     }
 
-    public function set($data, $tag)
+    public function getUnpackData($data, $tag)
     {
         if (isset($data[$tag]) && $data[$tag]['type'] == CoType::CO_STRING) {
             return $data[$tag]['val'];
+        }
+    }
+}
+
+class CoBool extends DataType
+{   
+    public function get(&$val, $tag, $isNeed = true)
+    {
+        $this->packVal($val, CoType::CO_BOOL, $tag, $isNeed);
+    }
+
+    public function getUnpackData($data, $tag)
+    {
+        if (isset($data[$tag]) && $data[$tag]['type'] == CoType::CO_BOOL) {
+            return boolval($data[$tag]['val']);
+        }
+    }
+}
+
+class CoList extends DataType
+{   
+    public function __construct($type)
+    {
+        $this->type = $type;
+    }
+    
+    public function get(&$val, $tag, $isNeed = true)
+    {
+        $this->packVal($val, CoType::CO_LIST, $tag, $isNeed);
+    }
+
+    public function setVal($data)
+    {   
+        if (!is_array($data)) {
+            throw new Exception("CO_LIST类型不匹配:{$data}", 1);
+        }
+
+        $this->val = null;
+
+        foreach ($data as $value) {
+            $struct = clone $this->type;
+            $this->val .= $struct->setVal($value);
+        }
+
+        $bodyLen = strlen($this->val);
+        $head = pack("N", $bodyLen);
+        $this->val = $head.$this->val;
+        return $this->val;
+    }
+
+    public function getUnpackData($data, $tag)
+    {
+        if (isset($data[$tag]) && $data[$tag]['type'] == CoType::CO_LIST) {
+            if (is_subclass_of($this->type, 'CoStruct')) {
+                $ret = [];
+                $one = $this->type->getVal($data[$tag]['val']);
+                //对每个结构体拆包
+                foreach ($one as $value) {
+                    $struct = clone $this->type;
+                    $unpackData = coUnpack($value);
+                    $struct->unpack($unpackData);
+                    $ret[] = $struct;
+                }
+
+                return $ret;
+            } else {
+                return $this->type->getVal($data[$tag]['val']);
+            }
         }
     }
 }
@@ -123,7 +220,7 @@ class CoStruct extends DataType
 {   
     public function get(&$val, $tag, $isNeed = true)
     {
-        $this->getVal($val, CoType::CO_STRUCT, $tag, $isNeed);
+        $this->packVal($val, CoType::CO_STRUCT, $tag, $isNeed);
     }
 
     public function setVal($data)
@@ -132,12 +229,13 @@ class CoStruct extends DataType
         $bodyLen = strlen($this->val);
         $head = pack("N", $bodyLen);
         $this->val = $head.$this->val;
+        return $this->val;
     }
 
-    public function set($data, $struct, $tag)
+    public function getUnpackData($data, $struct, $tag)
     {
         if (isset($data[$tag]) && $data[$tag]['type'] == CoType::CO_STRUCT) {
-            $unpackData = coPack($data[$tag]['val']);
+            $unpackData = coUnpack($data[$tag]['val']);
             $struct->unpack($unpackData);
 
             return $struct;
